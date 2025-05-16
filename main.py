869aaa35435366aef74c1c87c5041fb0e4b88bef
William Pabon <williamcodcpro@gmail.com>
2025-05-16 13:05:25 -0500

    feet:car:commit

--- main.py
+++ main.py
@@ -1,2 +1,4 @@
 from tabulate import tabulate
 
+table = [["1. Registrar nuevo gasto"],["2. Listar gastos"],["3. Calcular total de gastos"],["4. Generar reporte de gastos"],["5. Salir"]]
+print(tabulate(table, headers= [" Simulador de Gasto Diario"]))
